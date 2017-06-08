6e55fe987805fd89df40672c1a6f6b658268eb44
Peng <guoleii@gmail.com>
2017-06-08 17:08:09 +0800

    Initial Commit

---
+++ HelloVision/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloVision
+//
+//  Created by 郭朋 on 08/06/2017.
+//  Copyright © 2017 Peng Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
